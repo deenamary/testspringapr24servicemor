dd8d8268e9c40496c88e652a8c1b0a3b5242a057
deena <deenaantonym@gmail.com>
2024-05-06 23:45:56 +0530

    Initial commit

---
+++ src/main/java/com/example/testapr24servicemor/Testapr24servicemorApplication.java
@@ -0,0 +1,13 @@
+package com.example.testapr24servicemor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testapr24servicemorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testapr24servicemorApplication.class, args);
+    }
+
+}
