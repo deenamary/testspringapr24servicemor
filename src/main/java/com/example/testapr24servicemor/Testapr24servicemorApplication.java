6bbdde80816b6554cd8281d4d00256959b8c70bf
deena <deenaantonym@gmail.com>
2024-05-07 12:01:22 +0530

    Enabled service discovery

--- src/main/java/com/example/testapr24servicemor/Testapr24servicemorApplication.java
+++ src/main/java/com/example/testapr24servicemor/Testapr24servicemorApplication.java
@@ -2,8 +2,10 @@ package com.example.testapr24servicemor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Testapr24servicemorApplication {
 
     public static void main(String[] args) {
